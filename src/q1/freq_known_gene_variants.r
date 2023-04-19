172fe253d755c79db7b12c8fb9eb91e410811649
opiemartin <sarah.martin@kcl.ac.uk>
2023-04-19 10:45:21 +0100

    updated Rob's latest code

--- src/q1/freq_known_gene_variants.r
+++ src/q1/freq_known_gene_variants.r
@@ -2,154 +2,246 @@
 
 source("src/rmcf/inital.r")
 
-# Frequency of known gene variants
+###tested or not
+summary(ext_main$sod1_test_BOOL)
+summary(ext_main$c9orf72_test_BOOL)
+summary(ext_main$fus_tested_BOOL)
+summary(ext_main$tardbp_tested_BOOL)
+
+#Frequency of known gene variants
 summary(ext_main$sod1_stat_BOOL)
 summary(ext_main$c9orf72_stat_BOOL)
 summary(ext_main$fus_status_BOOL)
 summary(ext_main$tardbp_status_BOOL)
 
-# Filter by site
+#create a column where we have a total of out gene positives
+ext_main$gene_positive <- ifelse(ext_main$sod1_stat_BOOL | 
+                                   ext_main$c9orf72_stat_BOOL | 
+                                   ext_main$fus_status_BOOL | 
+                                   ext_main$tardbp_status_BOOL, TRUE, FALSE)
+summary(ext_main$gene_positive) #1127 with positive genetic mutation
+
+ext_main %>% 
+  filter(site != "King's") %>% 
+  ggplot() + 
+  geom_bar(mapping = aes(x = site, fill = gene_positive)) +
+  xlab("PRECISION ALS SITE") +
+  ylab("PATIENTS TESTED") +
+  scale_fill_discrete(name = "Genetic test result")
+
+#positive as a breakdown
+positive_breakdown <- data.frame(
+  gene = c("C9ORF72", "FUS", "TARDBP", 'SOD1'),
+  positive = c(sum(ext_main$c9orf72_stat_BOOL), 
+               sum(ext_main$fus_status_BOOL),
+               sum(ext_main$tardbp_status_BOOL),
+               sum(ext_main$tardbp_status_BOOL))
+)
+#Pie chart
+ggplot(positive_breakdown, aes(x = "", y = positive, fill = gene)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  geom_text(aes(label = positive), position = position_stack(vjust = 0.5)) +
+  labs(fill = "Gene")
+
+#Filter by site
 ext_main$site <- as.factor(ext_main$site)
 summary(ext_main$site)
 
 levels(ext_main$site)
-# BUG - I don't know why this doesn't work, will have to do individually
-for (x in c((levels(ext_main$site)))) {
-  ext_main %>%
-    filter(site == x, (c9orf72_stat_BOOL == TRUE |
-      fus_status_BOOL == TRUE |
-      tardbp_status_BOOL == TRUE |
-      sod1_stat_BOOL == TRUE)) %>%
-    select(
-      c9orf72_test_BOOL,
-      fus_status_BOOL,
-      tardbp_status_BOOL,
-      sod1_stat_BOOL
-    ) %>%
+#BUG - I don't know why this doesn't work, will have to do individually
+for (x in  c((levels(ext_main$site)))){
+  ext_main %>% 
+    filter(site == x , (c9orf72_stat_BOOL == TRUE |
+                          fus_status_BOOL == TRUE |
+                          tardbp_status_BOOL == TRUE|
+                          sod1_stat_BOOL == TRUE)) %>% 
+    select(c9orf72_test_BOOL, 
+           fus_status_BOOL, 
+           tardbp_status_BOOL, 
+           sod1_stat_BOOL) %>% 
     summary()
 }
 
-
-# Trinity
-ext_main %>%
-  filter(site == "Trinity", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+####Site specific####
+#Trinity
+ext_main %>% 
+  filter(site == 'Trinity', (c9orf72_stat_BOOL == TRUE |
+                               fus_status_BOOL == TRUE |
+                               tardbp_status_BOOL == TRUE|
+                               sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
 
-# Kings
-# Is there no genetic data available?
+#Kings
+#Is there no genetic data available?
 levels(ext_main$site)
-ext_main %>%
+ext_main %>% 
   filter(site == "King's", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                              fus_status_BOOL == TRUE |
+                              tardbp_status_BOOL == TRUE|
+                              sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
 
-# Bellvitge
-ext_main %>%
+#Bellvitge
+ext_main %>% 
   filter(site == "Bellvitge", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                                 fus_status_BOOL == TRUE |
+                                 tardbp_status_BOOL == TRUE|
+                                 sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
 
-# Karolinska
-ext_main %>%
+#Karolinska
+ext_main %>% 
   filter(site == "Karolinsk", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                                 fus_status_BOOL == TRUE |
+                                 tardbp_status_BOOL == TRUE|
+                                 sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-# Leuven
-ext_main %>%
+#Leuven
+ext_main %>% 
   filter(site == "Leuven", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                              fus_status_BOOL == TRUE |
+                              tardbp_status_BOOL == TRUE|
+                              sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-# Tours
-ext_main %>%
+#Tours
+ext_main %>% 
   filter(site == "Tours", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                             fus_status_BOOL == TRUE |
+                             tardbp_status_BOOL == TRUE|
+                             sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-# Sheffield
-ext_main %>%
+#Sheffield
+ext_main %>% 
   filter(site == "Sheffield", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                                 fus_status_BOOL == TRUE |
+                                 tardbp_status_BOOL == TRUE|
+                                 sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-# Turin
-ext_main %>%
+#Turin 
+ext_main %>% 
   filter(site == "Turin", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                             fus_status_BOOL == TRUE |
+                             tardbp_status_BOOL == TRUE|
+                             sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-# Utrecht
-ext_main %>%
+#Utrecht
+ext_main %>% 
   filter(site == "Utrecht", (c9orf72_stat_BOOL == TRUE |
-    fus_status_BOOL == TRUE |
-    tardbp_status_BOOL == TRUE |
-    sod1_stat_BOOL == TRUE)) %>%
-  select(
-    c9orf72_stat_BOOL,
-    fus_status_BOOL,
-    tardbp_status_BOOL,
-    sod1_stat_BOOL
-  ) %>%
+                               fus_status_BOOL == TRUE |
+                               tardbp_status_BOOL == TRUE|
+                               sod1_stat_BOOL == TRUE)) %>% 
+  select(c9orf72_stat_BOOL, 
+         fus_status_BOOL, 
+         tardbp_status_BOOL, 
+         sod1_stat_BOOL) %>% 
   summary()
-#### END####
+
+#What is the staging of each of the gene variants? spinal v bulbar in c9 for example
+
+c9_pos <- ext_main %>% 
+  filter(c9orf72_stat_BOOL == TRUE)
+
+summary(c9_pos$bulbar_onset)
+summary(c9_pos$respiratory_onset)
+summary(c9_pos$cognitive_onset)
+summary(c9_pos$spinal_onset)
+
+c9site <- data.frame(
+  site = c("Bulbar", "Respiratory", "Cognitive", 'Spinal'),
+  number = c(sum(ext_main$c9orf72_stat_BOOL), 
+             sum(ext_main$fus_status_BOOL),
+             sum(ext_main$tardbp_status_BOOL),
+             sum(ext_main$tardbp_status_BOOL)))
+
+ggplot(c9site, aes(x = "", y = number, fill = site)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  labs(fill = "Site of onset in C9ORF72")
+
+library(data.table)
+
+#earliest mitos stage
+ext_mitos <- ext_alsfrs |>
+  left_join(ext_main, by = "id") |>
+  transmute(
+    id = id,
+    date_of_assessment = date_of_assessment,
+    age_at_assessment = coalesce(
+      age_at_assessment, (date_of_assessment - date_of_birth) / dyears(1)
+    ),
+    mitos = {
+      walking_selfcare <- q8_walking <= 1 | q6_dressing_and_hygiene <= 1
+      swallowing <- q3_swallowing <= 1
+      communication <- q1_speech <= 1 | q4_handwriting <= 1
+      breathing <- q10_dyspnea <= 1 | q12_respiratory_insufficiency <= 2
+      walking_selfcare + swallowing + communication + breathing
+    }
+  ) |>
+  drop_na(id, mitos)
+
+ext_mitos <- ext_mitos %>%
+  group_by(id) %>%
+  mutate(time_since_first = as.numeric(difftime(date_of_assessment, min(date_of_assessment), 
+                                                units = "days"))/30)
+ext_mitos %>% 
+  filter(time_since_first == 0, c9orf72_stat_BOOL == TRUE) %>% 
+  ggplot(aes(x = mitos, fill = "red")) +
+  geom_bar() +
+  labs(title = 'MITOS score at diagnosis in C9ORF72',
+       x = "MITOS score",
+       y = "")
+
+#add c9 to mitos
+ext_mitos <- merge(ext_mitos, ext_main[,c("id", "c9orf72_stat_BOOL")], by="id")
+
+
+#earliest kings stage
+ext_kings <- ext_kings %>%
+  group_by(id) %>%
+  mutate(time_since_first = as.numeric(difftime(date_of_assessment, min(date_of_assessment), 
+                                                units = "days"))/30)  
+
+ext_kings %>% 
+  filter(time_since_first == 0) %>% 
+  ggplot(aes(x = kings)) +
+  geom_histogram()
+
+
+
+
+####END####
